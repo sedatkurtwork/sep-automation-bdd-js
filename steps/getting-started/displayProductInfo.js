727c95403c2219c0c7d2793b557bfbf5eaa40b16
sedatkurtwork <139935263+sedatkurtwork@users.noreply.github.com>
2025-11-19 16:35:34 -0500

    Add step definitions for product information display
    display product info steps file updated

--- steps/getting-started/displayProductInfo.js
+++ steps/getting-started/displayProductInfo.js
@@ -2,3 +2,28 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect} from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+
+Then(
+  "the product name should be displayed on the information card",
+  async function () {}
+);
+
+Then(
+  "the product name on the information card should match the product name on the left side of the screen",
+  async function () {}
+);
+
+Then("the price of the product should be displayed", async function () {});
+
+Then(
+  "the text indicating a flexible payment plan should be displayed",
+  async function () {}
+);
+
+Then("the program start date should be displayed", async function () {});
+
+Then("the return policy should be displayed", async function () {});
+
+Then("the final date for returns should be displayed", async function () {});
+
